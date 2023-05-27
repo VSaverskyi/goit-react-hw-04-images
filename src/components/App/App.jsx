b0666e2b85a3f00e6078c61a828736be38f40a68
VSaverskyi <vovikck15@gmail.com>
2023-05-27 15:57:04 +0200

    change App to function component with hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,24 @@
-import { Component } from "react";
+import React, {useState} from "react";
 import SearchBar from "../Searchbar";
 import { Container } from "./App.styled";
 import ImageGallery from "components/ImageGallery";
 
-class App extends Component {
-  state = {
-    searchingImage: '',
-  }
+const App = () => {
 
-  handleFormSubmit = searchingImage => {
-    this.setState({ searchingImage });
-  }
+  const [searchingImage, setSearchingImage] = useState('');
 
-  render() {
-    const {searchingImage} = this.state;
-    return (
-      <Container>
-        <SearchBar onSubmit={this.handleFormSubmit} />
-        <ImageGallery
-          searchingImage={searchingImage}
-        />
-      </Container>
+  const handleFormSubmit = searchingImage => {
+    setSearchingImage(searchingImage);
+  };
+
+  return (
+    <Container>
+      <SearchBar onSubmit={handleFormSubmit} />
+      <ImageGallery
+        searchingImage={searchingImage}
+       />
+    </Container>
     );
-  }
 };
 
 export default App;
