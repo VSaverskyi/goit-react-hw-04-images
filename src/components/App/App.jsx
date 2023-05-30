ba578085a94c77ba641faa19fdc88b1c4d59e770
VSaverskyi <vovikck15@gmail.com>
2023-05-30 19:14:20 +0200

    transfer of logic from ImageGallery to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,77 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import SearchBar from "../Searchbar";
 import { Container } from "./App.styled";
 import ImageGallery from "components/ImageGallery";
+import pixabayApi from '../../services/pixabay-api';
+
+const Status = {
+    IDLE: 'idle',
+    PENDING: 'pending',
+    RESOLVED: 'resolved',
+    REJECTED: 'rejected',
+};
+
+const perPage = 12;
 
 const App = () => {
 
   const [searchingImage, setSearchingImage] = useState('');
+  const [images, setImages] = useState([]);
+    const [error, setError] = useState(null);
+    const [status, setStatus] = useState(Status.IDLE);
+    const [buttonIsActive, setButtonIsActive] = useState(true);
+    const [pageNumber, setPageNumber] = useState(1);
 
   const handleFormSubmit = searchingImage => {
+    setPageNumber(1);
+    setImages([]);
     setSearchingImage(searchingImage);
   };
+
+    useEffect(() => {
+        if (searchingImage) {
+            const fetchResponse = async () => {
+            setStatus(Status.PENDING);
+              const response = await pixabayApi.fetchImages(searchingImage, pageNumber, perPage);
+              const totalPages = Math.floor(response.data.totalHits / perPage);
+            
+              if (pageNumber > totalPages) {
+                  setButtonIsActive(false);
+              }
+            
+              if (pageNumber === 1) {
+                setImages(response.data.hits);
+              } else {
+                setImages(prevImages => [...prevImages, ...response.data.hits]);
+              }
+            setStatus(Status.RESOLVED);
+        }
+
+            try {
+            fetchResponse();
+        }
+        catch (error) {
+            setError(error);
+            setStatus(Status.REJECTED);
+        }
+        } 
+
+    }, [searchingImage, pageNumber]);
+
+    const handleLoadMoreBtnClick = () => {
+        setPageNumber(prevPageNumber => prevPageNumber + 1);
+    };
 
   return (
     <Container>
       <SearchBar onSubmit={handleFormSubmit} />
       <ImageGallery
-        searchingImage={searchingImage}
+        status={status}
+        buttonIsActive={buttonIsActive}
+        images={images}
+        error={error}
+        pageNumber={pageNumber}
+        handleLoadMoreBtnClick={handleLoadMoreBtnClick}
        />
     </Container>
     );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import pixabayApi from '../../services/pixabay-api';
+import { useEffect } from "react";
 import ImagesDataView from "components/ImagesDataView";
 import ImagesPendingView from "components/ImagesPendingView";
 import ImagesErrorView from "components/ImagesErrorView";
@@ -12,15 +11,7 @@ const Status = {
     REJECTED: 'rejected',
 };
 
-const perPage = 12;
-
-const ImageGallery = ({searchingImage}) => {
-
-    const [images, setImages] = useState([]);
-    const [error, setError] = useState(null);
-    const [status, setStatus] = useState(Status.IDLE);
-    const [buttonIsActive, setButtonIsActive] = useState(true);
-    const [pageNumber, setPageNumber] = useState(1);
+const ImageGallery = ({status, buttonIsActive, images, error, handleLoadMoreBtnClick}) => {
 
     useEffect(() => {
         if (!buttonIsActive) {
@@ -28,54 +19,6 @@ const ImageGallery = ({searchingImage}) => {
         }
     },[buttonIsActive])
 
-    useEffect(() => {
-        setPageNumber(1);
-        if (searchingImage) {
-            const fetchResponse = async () => {
-            setStatus(Status.PENDING);
-            const response = await pixabayApi.fetchImages(searchingImage, 1, perPage);
-            setImages(response.data.hits);
-            setStatus(Status.RESOLVED);
-        }
-
-            try {
-            fetchResponse();
-        }
-        catch (error) {
-            setError(error);
-            setStatus(Status.REJECTED);
-        }
-        } 
-
-    }, [searchingImage]);
-
-    useEffect(() => {
-        if (pageNumber > 1 && searchingImage) {
-            const fetchResponseMore = async () => {
-                const response = await pixabayApi.fetchImages(searchingImage, pageNumber, perPage);
-                const totalPages = Math.floor(response.data.totalHits / perPage);
-            
-                if (pageNumber > totalPages) {
-                    setButtonIsActive(false);
-                }
-
-                const responseImages = response.data.hits;
-
-                setImages(prevImages => [...prevImages, ...responseImages]);
-            }
-
-            try {
-                fetchResponseMore();
-            } catch (error) {
-                return <ImagesErrorView message={error.message} />;
-            }
-        }
-    }, [pageNumber, searchingImage])
-    
-
-    const handleLoadMoreBtnClick = () => {
-        setPageNumber(prevPageNumber => prevPageNumber + 1);
-    };
 
     if (status === Status.PENDING) {
         return <ImagesPendingView />;
@@ -93,5 +36,11 @@ const ImageGallery = ({searchingImage}) => {
 export default ImageGallery;
 
 ImageGallery.propTypes = {
-    searchingImage: PropTypes.string.isRequired
+    status: PropTypes.string.isRequired,
+    buttonIsActive: PropTypes.bool.isRequired,
+    images: PropTypes.array.isRequired,
+    error: PropTypes.shape({
+      message: PropTypes.string.isRequired
+    }),
+    handleLoadMoreBtnClick: PropTypes.func.isRequired
 }
